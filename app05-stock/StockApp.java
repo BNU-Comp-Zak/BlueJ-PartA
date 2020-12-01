12b0921e93630f9d28961af98b61371440abab49
Zak Smith <71933689+Zak-Smith@users.noreply.github.com>
2020-12-01 13:13:41 +0000

    Changes

--- app05-stock/StockApp.java
+++ app05-stock/StockApp.java
@@ -1,38 +1,28 @@
-
 /**
  * This app provides a user interface to the
  * stock manager so that users can add, edit,
  * print and remove stock products
  *
- * @author Student Name
- * @version 0.1
+ * @author Zak Smith
+ * @version 1.0
  */
 public class StockApp
 {
+    public static final char CLEAR_CODE = '\u000c';
+    public static final String QUIT = "quit";
+    public static final String ADD = "add";
+    public static final String PRINT_ALL = "printall";
     // Use to get user input
-    private InputReader input;
+    private InputReader input = new InputReader();
     
-    /**
-     * Constructor for objects of class StockApp
-     */
-    public StockApp()
-    {
-        input = new InputReader();
-    }
-
+    private StockManager manager = new StockManager();
+    
+    private StockDemo demo = new StockDemo(manager);
+    
     /**
      * 
      */
     public void run()
-    {
-        printHeading();
-        getMenuChoice();
-    }
-    
-    /**
-     * 
-     */
-    public void getMenuChoice()
     {
         boolean finished = false;
         
@@ -41,12 +31,45 @@ public class StockApp
             printHeading();
             printMenuChoices();
            
-            String choice = input.getInput();
-            finished = true;
+            String choice = input.getString().toLowerCase();
+            if(choice.equals(QUIT))
+                finished = true;
+            else    
+                executeMenuChoice(choice);
         }
     }
     
-   
+    private void executeMenuChoice(String choice)
+    {
+        if(choice.equals(ADD))
+        {
+            addProduct();
+        }
+        else if(choice.equals(PRINT_ALL))
+        {
+            manager.printAllProducts();
+            String value = input.getString();
+        }
+    }
+    
+    private void addProduct()
+    {
+        System.out.println("Adding new product\n");
+        
+        System.out.println("Please enter the product ID");
+        String value = input.getString();
+        int id = Integer.parseInt(value);
+        
+        System.out.println("Please enter the name of the product");
+        String name = input.getString();
+        
+        Product product = new Product(id, name);
+        manager.addProduct(product);
+        
+        
+        System.out.println();
+    }
+    
     /**
      * Print out a menu of operation choices
      */
@@ -65,9 +88,10 @@ public class StockApp
      */
     private void printHeading()
     {
+        //System.out.println(CLEAR_CODE);
         System.out.println("******************************");
         System.out.println(" Stock Management Application ");
-        System.out.println("    App05: by Student Name");
+        System.out.println("      App05: by Zak");
         System.out.println("******************************");
     }
 }
---
+++ app05-stock/StockDemo.java
@@ -0,0 +1,111 @@
+/**
+ * Demonstrate the StockManager and Product classes.
+ * The demonstration becomes properly functional as
+ * the StockManager class is completed.
+ * 
+ * @author David J. Barnes and Michael Kölling.
+ * @version 2016.02.29
+ * 
+ * edited by: Zak Smith
+ * Date: 07/11/20
+ */
+public class StockDemo
+{
+    // The stock manager.
+    private StockManager manager;
+
+    /**
+     * Create a StockManager and populate it with a few
+     * sample products.
+     */
+    public StockDemo(StockManager manager)
+    {
+        this.manager = manager;
+        
+        manager.addProduct(new Product(100, "Clock Radio"));
+        manager.addProduct(new Product(101, "Mobile Phone"));
+        manager.addProduct(new Product(102, "Microwave Oven"));
+        manager.addProduct(new Product(103, "Samsung Galaxy S8"));
+        manager.addProduct(new Product(104, "Samsung Galaxy S10"));
+        manager.addProduct(new Product(105, "Samsung Galaxy S20"));
+        manager.addProduct(new Product(106, "Apple MacBook Pro"));
+    }
+    
+    /**
+     * Provide a very simple demonstration of how a StockManager
+     * might be used. Details of one product are shown, the
+     * product is restocked, and then the details are shown again.
+     */
+    public void demoDelivery()
+    {
+        // Show details of all of the products.
+        manager.printAllProducts();
+        // Take delivery of 5 items of one of the products.
+        manager.delivery(100, 5);
+        manager.delivery(101, 3);
+        manager.delivery(102, 6);
+        manager.delivery(103, 2);
+        manager.delivery(104, 7);
+        manager.delivery(105, 9);
+        manager.delivery(106, 1);
+        manager.printAllProducts();
+    }
+    
+    /**
+     * Show details of the given product. If found,
+     * its name and stock quantity will be shown.
+     * @param id The ID of the product to look for.
+     */
+    public void showDetails(int id)
+    {
+        Product product = getProduct(id);
+        
+        if(product != null) 
+        {
+            System.out.println(product.toString());
+        }
+    }
+    
+    /**
+     * Sell one of the given item.
+     * Show the before and after status of the product.
+     * @param id The ID of the product being sold.
+     */
+    public void sellProduct(int id)
+    {
+        Product product = getProduct(id);
+        
+        if(product != null) 
+        {
+            showDetails(id);
+            product.sellOne();
+            showDetails(id);
+        }
+    }
+    
+/**
+     * Get the product with the given id from the manager.
+     * An error message is printed if there is no match.
+     * @param id The ID of the product.
+     * @return The Product, or null if no matching one is found.
+     */
+    public Product getProduct(int id)
+    {
+        Product product = manager.findProduct(id);
+        
+        if(product == null) 
+        {
+            System.out.println("Product with ID: " + id +
+                               " is not recognised.");
+        }
+        return product;
+    }
+
+    /**
+     * @return The stock manager.
+     */
+    public StockManager getManager()
+    {
+        return manager;
+    }
+}
